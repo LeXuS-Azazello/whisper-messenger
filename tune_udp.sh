07d8a4fde75f4d999163a65a7f119c0e19abbf35
Soldatoff <lexus@debug.org.ua>
2026-04-17 05:46:03 +0700

    tune up.sh

---
+++ tune_udp.sh
@@ -0,0 +1,6 @@
+# cloudflared quick fix for QUIC UDP buffer error
+# Run as root to increase UDP buffer sizes
+sysctl -w net.core.rmem_max=67108864
+sysctl -w net.core.wmem_max=67108864
+ulimit -n 1048576
+echo "Tuned UDP buffers and file descriptors"
